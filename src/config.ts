ac8c41dbd2b65e542a9902ec1b291f33f31cf9f1
Anurag Roy <anuragroy@duck.com>
2023-04-27 10:00:36 +0530

    Update stocks

--- src/config.ts
+++ src/config.ts
@@ -26,6 +26,7 @@ export const GROUPS = {
     'SAIL',
     'NTPC',
     'RECLTD',
+    'IRCTC',
   ],
   'Banking & Finance, Automotive & Spares': [
     'IBULHSGFIN',
@@ -55,6 +56,7 @@ export const GROUPS = {
     'DIVISLAB',
     'DRREDDY',
     'IPCALAB',
+    'CROMPTON',
   ],
 };
 
